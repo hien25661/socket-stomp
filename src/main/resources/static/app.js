79d527fa9288a73922ca8debebfdefe5f626b215
Hiennguyen <hiennv@its-j.com>
2019-05-10 14:50:17 +0900

    Init Code

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,57 @@
+var stompClient = null;
+//todo need to get number message unread from database, default 0 just example
+var numberMessage = 0;
+
+
+function setConnected(connected){
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+
+    if(connected){
+        $("#conversation").show();
+    }else {
+        $("#conversation").hide();
+    }
+
+    $("#greetings").html("");
+
+}
+
+function connect(){
+    var socket = new SockJS("/gs_guide-websocket");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log("connected: " + frame);
+        stompClient.subscribe("/topic/greetings", function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect(){
+    if(stompClient != null){
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName(){
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    numberMessage++;
+    $("#greetings").append("<tr><td>" + numberMessage + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
